85cf5040cd0eca382a4caff6bc9ce42a9faeba42
Daekuen <daekuenhan@gmail.com>
2022-09-14 19:35:19 +0900

    navbar JS

---
+++ main.js
@@ -0,0 +1,13 @@
+"use strict";
+
+// navbar 투명에서 스크롤 내리면 진해지도록 만듬.
+
+const navbar = document.querySelector("#navbar");
+const navbarHeight = navbar.getBoundingClientRect().height;
+document.addEventListener("scroll", () => {
+  if (window.scrollY > navbarHeight) {
+    navbar.classList.add("navbar--dark");
+  } else {
+    navbar.classList.remove("navbar--dark");
+  }
+});
